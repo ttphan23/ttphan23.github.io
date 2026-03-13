92b724a38d7041bdd63bad9e5e5bd4e9faf30edd
Thinh Phan <phantyson@gmail.com>
2026-03-13 09:25:11 -0400

    adding styling to portfolio structure

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const menuToggle = document.getElementById("menuToggle");
+const siteNav = document.getElementById("siteNav");
+
+if (menuToggle && siteNav) {
+  menuToggle.addEventListener("click", () => {
+    siteNav.classList.toggle("show");
+  });
+}
